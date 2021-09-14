3b88f3438773e93e04cf9469770588b86623f400
“prit108” <pritgodhani132@gmail.com>
2021-09-15 01:09:05 +0530

    Q1 Final Edits Complete

--- Assignment-3/booth.c
+++ Assignment-3/booth.c
@@ -3,28 +3,50 @@
 
 // booth's multiplication procedure
 int main() {
-    int A,B;
-    int prod = 0;
+    int M,Q;
+    int A = 0;
     printf("Enter the first number: ");
-    scanf("%d", &A);
+    scanf("%d", &M);
     printf("\nEnter the second number: ");
-    scanf("%d", &B);
-    prod += B;
-    int aux_bit = 0;
+    scanf("%d", &Q);
+    int A_ = M, B_ = Q;
+    int prod = B_;
+    int aux_bit = 0, aux_bit_ = 0;
     int cnt = 16;
 
     while(cnt > 0) {
-        if((prod&1) && !aux_bit) {
-            prod -= (A<<16);
+        // augmented A:Q procedure
+        if((prod&1) && !aux_bit_) {
+            prod -= (A_<<16);
         }
-        else if(!(prod&1) && aux_bit) {
-            prod += (A<<16);
+        else if(!(prod&1) && aux_bit_) {
+            prod += (A_<<16);
         }
-        aux_bit = (prod&1);
+        aux_bit_ = (prod&1);
         prod >>= 1;
-        cnt--;
-    }
 
-    printf("\nThe product of the two numbers is: %d\n", prod);
+
+        // separated A, Q procedure
+        if((Q&1) && !aux_bit) {
+            A -= M;
+        }
+        else if(!(Q&1) && aux_bit) {
+            A += M;
+        }
+        aux_bit = (Q&1);
+        Q >>= 1;
+        int lastofA = (A&1);
+        A >>= 1;
+        if(lastofA) {
+            Q = Q | (0x8000);
+        } else {
+            Q = Q & (0x7fff);
+        }
+        cnt--;
+        printf("Itr: %d, PROD = %x, AUX_ = %d, A = %x, Q = %x, AUX = %d \n", cnt, prod, aux_bit_, A, Q, aux_bit);
+    } 
+    int finalans = (A<<16) + Q;
+
+    printf("\nThe product of the two number through prod is: %d and through A-Q is : %d\n",prod,finalans);
     return 0;
 }
