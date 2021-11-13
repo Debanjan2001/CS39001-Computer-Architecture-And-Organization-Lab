fe41e4ec0f87ce945fd7aae8bf837b50361c7041
prit108 <63956555+prit108@users.noreply.github.com>
2021-11-13 06:24:32 +0530

    IGNORE::Uncommited changes in isim files.

--- KGP_RISC/ass7_kgp_risc/isim/TB_risc_isim_beh.exe.sim/work/m_11335005060465295095_4017027212.c
+++ KGP_RISC/ass7_kgp_risc/isim/TB_risc_isim_beh.exe.sim/work/m_11335005060465295095_4017027212.c
@@ -21,48 +21,11 @@
 #include <malloc.h>
 #define alloca _alloca
 #endif
-static const char *ng0 = "/home/ise/shared_xlnx/CS39001-Computer-Architecture-And-Organization-Lab/KGP_RISC/ass7_kgp_risc/risc.v";
 
 
 
-static void Cont_17_0(char *t0)
+
+extern void work_m_11335005060465295095_4017027212_init()
 {
-    char *t1;
-    char *t2;
-    char *t3;
-    char *t4;
-    char *t5;
-    char *t6;
-    char *t7;
-    char *t8;
-
-LAB0:    t1 = (t0 + 5400U);
-    t2 = *((char **)t1);
-    if (t2 == 0)
-        goto LAB2;
-
-LAB3:    goto *t2;
-
-LAB2:    xsi_set_current_line(17, ng0);
-    t2 = (t0 + 4088U);
-    t3 = *((char **)t2);
-    t2 = (t0 + 5800);
-    t4 = (t2 + 56U);
-    t5 = *((char **)t4);
-    t6 = (t5 + 56U);
-    t7 = *((char **)t6);
-    memcpy(t7, t3, 8);
-    xsi_driver_vfirst_trans(t2, 0, 31);
-    t8 = (t0 + 5720);
-    *((int *)t8) = 1;
-
-LAB1:    return;
-}
-
-
-extern void work_m_00998983300911243969_4017027212_init()
-{
-	static char *pe[] = {(void *)Cont_17_0};
-	xsi_register_didat("work_m_00998983300911243969_4017027212", "isim/TB_risc_isim_beh.exe.sim/work/m_00998983300911243969_4017027212.didat");
-	xsi_register_executes(pe);
+	xsi_register_didat("work_m_11335005060465295095_4017027212", "isim/TB_risc_isim_beh.exe.sim/work/m_11335005060465295095_4017027212.didat");
 }
